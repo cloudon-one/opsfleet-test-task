282c62c16c8a707d6565e24f3f640bb44dca1017
ynaumenko <ynaumenko@outlook.com>
2024-08-15 13:32:28 +0300

    karpenter api update

--- eks-karpenter/terraform/karpenter.tf
+++ eks-karpenter/terraform/karpenter.tf
@@ -12,24 +12,37 @@ module "karpenter" {
 
 resource "kubectl_manifest" "karpenter_provisioner" {
   yaml_body = <<-YAML
-apiVersion: karpenter.sh/v1alpha5
-kind: Provisioner
+apiVersion: karpenter.sh/v1beta1
+kind: NodePool
 metadata:
-  name: default
+ name: default-nodepool
 spec:
-  requirements:
-    - key: karpenter.sh/capacity-type
-      operator: In
-      values: ["on-demand"]
-    - key: kubernetes.io/arch
-      operator: In
-      values: ["amd64", "arm64"]
+  template:
+    spec:
+      requirements:
+        - key: kubernetes.io/arch
+          operator: In
+          values: ["amd64"]
+        - key: kubernetes.io/os
+          operator: In
+          values: ["linux"]
+        - key: karpenter.sh/capacity-type
+          operator: In
+          values: ["on-demand"]
+        - key: karpenter.k8s.aws/instance-family
+          operator: In
+          values: ["t3"]
+        - key: karpenter.k8s.aws/instance-size
+          operator: In
+          values: ["medium","large", "xlarge"]
+        - key: karpenter.k8s.aws/instance-generation
+          operator: Gt
+          values: ["2"]
+      nodeClassRef:
+        name: default
   limits:
-    resources:
-      cpu: 1000
-  provider:
-    instanceTypes: ["t3.medium", "t4g.medium"]
-  ttlSecondsAfterEmpty: 30
+    cpu: "80"
+    memory: "320Gi"
 YAML
 
   depends_on = [module.karpenter]
